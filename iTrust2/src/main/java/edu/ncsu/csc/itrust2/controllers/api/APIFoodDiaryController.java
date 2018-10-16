01ea25f6e9348d1253b9ada6509c025ad5000011
Noah Benveniste <nnbenven@ncsu.edu>
2018-10-16 15:08:21 -0400

    Issue #24: fixed a fatal bug where diary entries could not be added by a patient if they did not have entries already in the database. This was due to the fact that the patient name field in the DiaryEntryForm was dependent on there being a patient name passed back from the front end, but the frontend was getting the name of the logged in patient from its list of diary entries. To fix this, I updated the method for handling patient POST requests in the API controller to call LoggerUtil.currentUser() to set the username field in the DiaryEntryForm. The only consequence of this is that we won't be able to display the patient's name on the frontend from their view of the food diary. Also added a line to HibernateDataGenerator to say when it's finished because we'll need to run this before every run of our cucumber tests.

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/APIFoodDiaryController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/APIFoodDiaryController.java
@@ -131,6 +131,8 @@ public class APIFoodDiaryController extends APIController {
     @PostMapping ( BASE_PATH + "/diaryentries" )
     public ResponseEntity createDiaryEntry ( @RequestBody final DiaryEntryForm entryF ) {
         try {
+            // Gets the currently logged in user
+            entryF.setPatient( LoggerUtil.currentUser() );
             final DiaryEntry entry = new DiaryEntry( entryF );
             if ( null != DiaryEntry.getById( entry.getId() ) ) {
                 return new ResponseEntity(
